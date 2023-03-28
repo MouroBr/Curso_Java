45521cf2154fcdb88ac8b573de050915dc3b6a7a
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-03-28 00:26:05 -0300

    Tipos primitivos

---
+++ src/src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 10;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0D;
+        float salarioFLoat = 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        String nome = "Goku";
+
+        System.out.println("A idade é"+age+" anos");
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+        System.out.println("Oi meu nome é "+nome);
+    }
+
+}
