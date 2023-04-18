7968dee3cc1645d914203142bfdd4c48b0d5eeba
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-04-18 19:27:20 -0300

    Tipos primitivos

---
+++ src/src/academy/devdojo/maratonajava/introducao/ExercicioTiposPrimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class ExercicioTiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Bruno Brizolla Do Nascimento";
+        String endereco = "Rua Da Praia n°250";
+        double salario = 2000;
+        String datadoPagamento = "28/03/2023";
+        String mensagem = "Eu "+nome+", morador do endereço "+endereco+", confirmo o recebimento da importancia de R$"+salario+", referente ao salário na data"+datadoPagamento+".";
+
+        System.out.println(mensagem);
+    }
+
+
+
+}
--- src/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
     public static void main(String[]args)
+
     {
         System.out.println("Hello Word");
     }
