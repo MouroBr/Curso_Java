1457960954847dc90a506a4539b73e790bf91eea
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-04-18 20:06:38 -0300

    exercicios tipos primitivos

--- src/src/academy/devdojo/maratonajava/introducao/ExercicioTiposPrimitivos.java
+++ src/src/academy/devdojo/maratonajava/introducao/ExercicioTiposPrimitivos.java
@@ -5,7 +5,7 @@ public class ExercicioTiposPrimitivos {
         String nome = "Bruno Brizolla Do Nascimento";
         String endereco = "Rua Da Praia n°250";
         double salario = 2000;
-        String datadoPagamento = "28/03/2023";
+        String datadoPagamento = "29/03/2023";
         String mensagem = "Eu "+nome+", morador do endereço "+endereco+", confirmo o recebimento da importancia de R$"+salario+", referente ao salário na data"+datadoPagamento+".";
 
         System.out.println(mensagem);
