ffe3b600ef13fa1e28cd64be95a12a3e3eb74232
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-04-27 21:58:06 -0300

    Módulo sobre estruturas de repetição(while, do while, for)

---
+++ src/src/academy/devdojo/maratonajava/introducao/EstruturasLacoRepeticao01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasLacoRepeticao01 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 10){
+            System.out.println(++count);
+
+        }
+        do {
+            System.out.println("Dentro do do-while");
+        } while (count < 10);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For "+ i);
+        }
+    }
+}
